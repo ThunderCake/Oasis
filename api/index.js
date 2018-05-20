05ce4803ac025c3f60027a948f69d1f46b70fa17
neo492 <neox871@gmail.com>
2018-05-20 23:26:40 +0200

    Add settigns get and post

--- api/index.js
+++ api/index.js
@@ -1,23 +1,57 @@
 const express = require('express');
-const app = express();
+const api = express();
 const nedb = require('nedb');
 const { promisify } = require('util');
 const { hosts } = require('../shared/db');
+const { settings } = require('../shared/db');
+
 
 const PORT = 4242;
-app.use(express.json());
+api.use(express.json());
 
-app.get('/admin/hosts', async (req, res) => {
-    const result = await hosts.findAsync({}).catch(e => console.log(e));
-    res.send(result);
+api.get('/hosts', async (req, res) => {
+    const results = await hosts.findAsync({}).catch(e => e.message);
+    res.send(results);
 });
 
-app.post('/admin/add/host', async (req, res) => {
+api.post('/hosts/add', async (req, res) => {
     const posts = req.body;
     await hosts.ensureIndexAsync({ fieldName: 'domain', unique: true });
-    const query = await hosts.insertAsync(posts);
+    try {
+
+        const query = await hosts.insertAsync(posts);
+
+        res.status(200);
+        return res.json(query);
+    } catch(e) {
+        res.status(400);
+        return res.json({"error" : e.message});
+    }
+
 });
 
-app.listen(PORT, () => {
+api.get('/settings', async(req, res) => {
+    const results = await settings.findAsync({});
+    
+    res.json(results);
+});
+
+api.post('/settings/add', async(req, res) => {
+    const params = req.body;
+    await settings.ensureIndexAsync({ fieldName: 'key', unique: true });
+    try {
+
+        const query = await settings.insertAsync(params);
+
+        res.status(200);
+        return res.json(query);
+    } catch(e) {
+        res.status(400);
+        return res.json({"error" : e.message});
+    }
+
+});
+
+api.listen(PORT, () => {
     console.log(`Server started on http://0.0.0.0:${PORT}`);
 });
