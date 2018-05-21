35832b84ee1f1ba2a7c0af65d7356b7f55202363
Felix Yadomi <dev.yadomi@gmail.com>
2018-05-21 16:49:45 +0200

    Add grommet UI

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from 'react-router-dom'
-import { App } from './components'
+import { App as Main } from './components'
+import 'grommet/grommet.min.css';
+
+import App from 'grommet/components/App'
 
 if (module.hot) module.hot.accept();
 
 ReactDOM.render((
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <App>
+        <BrowserRouter>
+            <Main />
+        </BrowserRouter>
+    </App >
 ), document.getElementById("root"));
